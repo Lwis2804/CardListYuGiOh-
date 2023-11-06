0fa1803b30bbfe1ed71a2ee7ec603e2a511ce5ba
LUIS <luis.enrique.enc@gmail.com>
2023-11-06 14:52:17 -0600

    [ADD] creación de vistas y celdas

---
+++ CartasYuGiHo/TableCells/CartasMagicas/CartasMagicasTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartasMagicasTableViewCell.swift
+//  CartasYuGiHo
+//
+//  Created by LUIS GONZALEZ on 06/11/23.
+//
+
+import UIKit
+
+class CartasMagicasTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
